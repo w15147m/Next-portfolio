466a43cb3255d09d502c2f95c5410ee52887ba65
waseem abbas <waseemofficee@gamil.com>
2026-07-22 21:30:36 +0500

    refactor: remove unused component files and clean up site page layout

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,26 +1,9 @@
 import React from 'react'
-import Hero from './_components/Hero'
-import TrustedBy from './_components/TrustedBy'
-import TechStack from './_components/TechStack'
-import Features from './_components/Features'
-import FeaturesTab from './_components/FeaturesTab'
-import Dashboards from './_components/Dashboards'
-import Plugins from './_components/Plugins'
-import CTA from './_components/CTA'
-import Blogs from './_components/Blogs'
 
 function page() {
   return (
     <main className="flex flex-col w-full overflow-hidden">
-      <Hero />
-      <TrustedBy />
-      <TechStack />
-      <Features />
-      <FeaturesTab />
-      <Dashboards />
-      <Plugins />
-      <CTA />
-      <Blogs />
+
     </main>
   )
 }
