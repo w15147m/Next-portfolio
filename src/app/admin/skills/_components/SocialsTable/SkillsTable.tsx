6da5ca12585ab15a616ded3bb2f9e0ac53fa4af4
waseem abbas <waseemofficee@gamil.com>
2026-07-22 18:36:29 +0500

    feat: modularize skills table body into separate components for improved maintainability

--- src/app/admin/skills/_components/SocialsTable/SkillsTable.tsx
+++ src/app/admin/skills/_components/SocialsTable/SkillsTable.tsx
@@ -6,6 +6,7 @@ import { Table } from "@/components/ui/table";
 import Loading from "@/components/ui/loaders/Loading";
 import { useSkills } from "../../_lib/useSkills";
 import SkillsTableHeader from "./component/SkillsTableHeader";
+import SkillsTableBody from "./component/SkillsTableBody/SkillsTableBody";
 
 interface SocialsTableProps {
   userId: string;
@@ -24,6 +25,7 @@ export default function SkillsTable({ userId }: SocialsTableProps) {
         <div className="overflow-x-auto">
             <Table>
               <SkillsTableHeader headers={headers} />
+              <SkillsTableBody userId={userId} skills={skills} isError={isError} />
           </Table>
         </div>
       )}
